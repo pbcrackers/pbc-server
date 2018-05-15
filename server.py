a9f5bc337d258a93897b889ee653bab400efd758
Erik Nelson <erik.david.nelson@gmail.com>
2018-05-15 14:20:58 -0400

    Trigger questions on channel every 30 seconds

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 import pusher
+import json
 import yaml
+import time
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -10,17 +12,28 @@ load_dotenv(dotenv_path=env_path, verbose=True)
 print('starting server')
 
 pusher_client = pusher.Pusher(
-  app_id=os.getenv("PUSHER_APP_ID"),
-  key=os.getenv("PUSHER_KEY"),
-  secret=os.getenv("PUSHER_SECRET"),
-  cluster=os.getenv("PUSHER_CLUSTER"),
-  ssl=True
+    app_id=os.getenv("PUSHER_APP_ID"),
+    key=os.getenv("PUSHER_KEY"),
+    secret=os.getenv("PUSHER_SECRET"),
+    cluster=os.getenv("PUSHER_CLUSTER"),
+    ssl=True
 )
 
 with open("questions.yaml", 'r') as stream:
-  try:
-    print(yaml.safe_load(stream))
-  except yaml.YAMLError as e:
-    print("Failed to parse questions file")
-    print(e)
+    try:
+        questions = yaml.safe_load(stream)['questions']
+        print(questions)
+    except yaml.YAMLError as e:
+        print("Failed to parse questions file")
+        print(e)
+        exit
 
+for q in questions:
+    print('asking question ' + q['question'])
+    pusher_client.trigger(
+        'questions', 'ask-question',
+        json.dumps({
+            'question': q['question'],
+            'answers': q['answers']
+        }))
+    time.sleep(30)
